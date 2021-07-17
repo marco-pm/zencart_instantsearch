b10c7f68e78e20907c1081344d29a519d25cd272
Marco Ponchia <marco.ponchia@marcopm.net>
2021-07-17 08:03:52 +0200

    Initial commit

---
+++ INSTALL/includes/classes/ajax/zcAjaxInstantSearch.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package Instant Search Results
+ * @copyright Copyright Ayoob G 2009-2011
+ * @copyright Portions Copyright 2003-2006 The Zen Cart Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: searches.php 5 2018-09-01 18:34:47Z davewest $
+ * 
+ * Instant Search+ 1.0.0
+ */
+
+class zcAjaxInstantSearch extends base
+{
+    protected const MAX_WORDSEARCH_LENGTH = 100;
+    protected const MAX_RESULTS           = 5;
+
+    public function instantSearch()
+    {
+        global $db;
+
+        $wordSearch = ($_POST['query'] ?? '');
+        $results    = [];
+
+        if ($wordSearch !== '' && strlen($wordSearch) < self::MAX_WORDSEARCH_LENGTH) { // if not empty and not too long
+
+            if (strlen($wordSearch) <= 2) {
+                $wordSearchPlus = "^" . $wordSearch;
+                $wordSearchPlusArray = [$wordSearch];
+            } else {
+                $wordSearchPlus = trim(preg_replace('/\s+/', ' ', $wordSearch));
+                $wordSearchPlusArray = explode(' ', $wordSearchPlus);
+                $wordSearchPlus = preg_replace('/\s/', '|', $wordSearchPlus);
+            }
+
+            $sqlProduct = "SELECT p.products_id, p.products_model, p.products_image, pd.products_name, pd.products_viewed
+                           FROM " . TABLE_PRODUCTS_DESCRIPTION . " as pd, " . TABLE_PRODUCTS . " as p 
+                           WHERE p.products_id = pd.products_id
+                           AND p.products_status <> 0
+                           AND ((pd.products_name REGEXP :wordSearchPlus:) OR (p.products_model REGEXP :wordSearchPlus:) OR (LEFT(pd.products_name, LENGTH(:wordSearch:)) SOUNDS LIKE :wordSearch:))
+                           AND language_id = '" . (int)$_SESSION['languages_id'] . "'";
+
+            $sqlProduct = $db->bindVars($sqlProduct, ':wordSearch:', $wordSearch, 'string');
+            $sqlProduct = $db->bindVars($sqlProduct, ':wordSearchPlus:', $wordSearchPlus, 'string');
+
+            $dbProducts = $db->Execute($sqlProduct);
+
+            if ($dbProducts->RecordCount() > 0) {
+
+                foreach ($dbProducts as $dbProduct) {
+                    $productId       = $dbProduct['products_id'];
+                    $productName     = $dbProduct['products_name'];
+                    $productModel    = $dbProduct['products_model'];
+                    $productImg      = $dbProduct['products_image'];
+                    $productViews    = $dbProduct['products_viewed'];
+                    $totalMatches    = 0;
+                    $findSum         = null; // sum of first occurrences of words in the product name
+
+                    // check if product model is an exact match
+                    if (strtolower(trim(preg_replace('/\s+/', ' ', $productModel))) === strtolower(trim(preg_replace('/\s+/', ' ', $wordSearch)))) {
+                        $totalMatches++;
+                    }
+
+                    foreach ($wordSearchPlusArray as $word) {
+                        $wordPos = stripos($productName, $word);
+                        
+                        if ($wordPos !== false) { // search for word anywhere in the product name
+                            $totalMatches++;
+                            $findSum += $wordPos;
+
+                            if (preg_match("/\b$word\b/i", $productName)) { // exact words matches have a higher priority
+                                $totalMatches++;
+                            }
+                        } elseif (stripos($productModel, $word) === 0) { // search for word at the beginning of the product model
+                            $totalMatches++;
+                        }
+                    }
+
+                    $result = [
+                        'name'  => strip_tags($productName),
+                        'link'  => zen_href_link(zen_get_info_page($productId), 'products_id=' . $productId),
+                        'model' => $productModel,
+                        'img'   => zen_image(DIR_WS_IMAGES . strip_tags($productImg), strip_tags($productName)),
+                        'srch'  => $wordSearchPlus,
+                        'mtch'  => $totalMatches,
+                        'views' => $productViews,
+                        'fsum'  => $findSum ?? self::MAX_WORDSEARCH_LENGTH
+                    ];
+                    $results[] = $result;
+                }
+
+                // order by number of matches (desc),
+                // then by words that occur first in the title,
+                // then by number of views (desc)
+                usort($results, static fn($prod1, $prod2) =>
+                    [$prod2['mtch'], $prod1['fsum'], $prod2['views']]
+                    <=>
+                    [$prod1['mtch'], $prod2['fsum'], $prod1['views']]
+                );
+            }
+        }
+
+        return array_slice($results, 0, self::MAX_RESULTS);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,21 +1,14 @@
-# Instant Search v1.0.2
-Just like googles instant search feature I’ve made one for zen cart. Instant search is a new search enhancement that shows results as you type. 
+# Instant Search+ 1.0.0
+Instant Search for Zen Cart, based on [Instant Search](https://github.com/torvista/InstantSearch) plugin.
 
-Tried and tested and works on all major browsers including smart phones.
+Tested on Zen Cart 1.5.7c (responsive classic template), PHP 7.4.
 
-This contribution is subject to version 2.0 of the GPL license, that is bundled with this package in the file LICENSE, and is available here.
+* Replaces `searches.php` with an AJAX class
+* Searches into the product name and the product model
+* Better, per-word search
+* Better sorting logic of the results
+* Displays the product image, name and model on the result
 
-These files are submitted for public distribution via the Zen Cart forum. It would be great if you can provide your feedback or support via the support thread at the Zen Cart forum where it can benefit all users of this add-on module.
-
-How It Works
- * When the page loads up jscript_instantSearch.js adds a keyup listeners to the search boxes.
- * When the user starts to key in a few letters jscript_instantSearch.js sends the data to searches.php.
- * searches.php uses sql to gather matching search results.
- * searches.php sends these results back to jscript_instantSearch.js.
- * jscript_instantSearch.js then creates the instant search box with the results on the web page.
-
- * Instant search uses the jQuery JavaScript Library. 
- * You can change the style and layout of the instant search via stylesheet_instantSearch.css.
- 
- 
-Unmodified upload..  Still planning on adding and editing.
+## TODO
+* Search also within categories and manufacturers
+* Admin configuration (e.g. enable/disable, max # of results, which fields to search, which fields to show (e.g. price))
