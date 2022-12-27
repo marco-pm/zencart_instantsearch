a9be88e259c4f6dfda4899661a7d874552803c37
Marco Ponchia <marco.ponchia@marcopm.net>
2022-12-27 08:00:27 +0100

    Use a single result container in the DOM and improve auto-positioning

--- includes/templates/YOUR_TEMPLATE/jscript/instant_search.js
+++ includes/templates/YOUR_TEMPLATE/jscript/instant_search.js
@@ -10,6 +10,7 @@
 
 const {slideDown, slideUp, slideToggle} = window.domSlider
 const searchBoxSelector = 'input[name="keyword"]';
+const resultsContainerSelector = 'instantSearchResultsContainer';
 let controller;
 let inputBoxCurrent;
 let inputTimer;
@@ -18,39 +19,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputBox = document.querySelectorAll(searchBoxSelector);
 
     for (let i = 0; i < inputBox.length; i++) {
-        let offset = inputBox[i].getBoundingClientRect();
-        let resultsContainer = document.createElement('div');
-
         inputBox[i].setAttribute('autocomplete', 'off');
 
-        // add a results container to the DOM for each search input
-        resultsContainer.setAttribute('id', 'instantSearchResultsContainer' + i);
-        resultsContainer.setAttribute('class', 'instantSearchResultsContainer');
-        resultsContainer.style.left = offset.left + window.scrollX + 'px';
-        resultsContainer.style.top = offset.top + window.scrollY + inputBox[i].clientHeight + 'px';
-        document.body.appendChild(resultsContainer);
-
-        // hide the results container on blur
+        // Hide the results container on blur
         inputBox[i].addEventListener('blur', function() {
-            if (inputBoxCurrent) {
-                const resultsContainer = document.querySelector(`#instantSearchResultsContainer${i}`);
-                slideUp({element: resultsContainer, delay: 300, slideSpeed: 200});
-            }
+            removeResultsContainer();
         });
 
-        // perform the search and shows the results container on input and focus
+        // Perform the search and shows the results container on input and focus
         ['input', 'focus'].forEach(event => inputBox[i].addEventListener(event, function() {
             inputBoxCurrent = inputBox[i];
-            const resultsContainer = document.querySelector(`#instantSearchResultsContainer${i}`);
-            const typedSearchWord = this.value;
 
+            const typedSearchWord = this.value;
             let searchWord = typedSearchWord.replace(/^\s+/, "").replace(/  +/g, ' ');
+
             if (searchWord === "" || searchWord.length < searchInputMinLength) {
-                resultsContainer.style.display = 'none';
+                removeResultsContainer();
             } else {
                 if (controller) {
                     controller.abort();
                 }
+
                 clearTimeout(inputTimer);
                 inputTimer = setTimeout(async () => {
                     controller = new AbortController();
@@ -62,26 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         const response = await fetch('ajax.php?act=ajaxInstantSearch&method=instantSearch', {
                             method: 'POST',
                             headers: {
-                                "X-Requested-With": "XMLHttpRequest",
+                                'X-Requested-With': 'XMLHttpRequest',
                             },
                             body: data,
                             signal
                         });
                         const responseData = await response.json();
-                        if (responseData.length > 0) {
-                            resultsContainer.innerHTML = responseData;
-                            if (inputBoxCurrent.value === typedSearchWord) {
-                                autoPositionContainer(inputBoxCurrent, resultsContainer);
-                                slideDown({element: resultsContainer, slideSpeed: 200});
-                            }
-                            resultsContainer.style.width = inputBoxCurrent.offsetWidth + 'px';
-                            if (resultsContainer.offsetWidth > 250) {
-                                resultsContainer.classList.add('instantSearchResultsContainer--lg');
-                            } else {
-                                resultsContainer.classList.remove('instantSearchResultsContainer--lg');
-                            }
+                        if (responseData.length > 0 && inputBoxCurrent.value === typedSearchWord) {
+                            createAndPositionResultsContainer(inputBoxCurrent, responseData);
+                            await slideDown({element: document.querySelector(`#${resultsContainerSelector}`), slideSpeed: 200});
                         } else {
-                            resultsContainer.style.display = 'none';
+                            removeResultsContainer();
                         }
                     } catch (e) {
                         if (e instanceof DOMException && e.name === "AbortError") {
@@ -96,20 +76,52 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-function autoPositionContainer(inputBox, container) {
-    const offsetInputBox = inputBox.getBoundingClientRect();
-    const inputBoxLeft = offsetInputBox.left + window.scrollX;
-    const inputBoxTop = offsetInputBox.top + window.scrollY;
-    const xOverflow = inputBoxLeft + container.offsetWidth;
-    const winWidth = document.documentElement.clientWidth;
-
-    let leftVal;
-    if (xOverflow > winWidth) {
-        let dif = xOverflow - winWidth;
-        leftVal = (((inputBoxLeft - dif) < 0) ? 0 : (inputBoxLeft - dif));
+// Create, populate and position the results container below the current input box
+function createAndPositionResultsContainer(inputBox, resultsData) {
+    if (document.querySelector(`#${resultsContainerSelector}`)) {
+        document.querySelector(`#${resultsContainerSelector}`).innerHTML = resultsData
     } else {
-        leftVal = inputBoxLeft;
+        const resultsContainer = document.createElement('div');
+        resultsContainer.innerHTML = resultsData;
+        resultsContainer.setAttribute('id', resultsContainerSelector);
+        resultsContainer.setAttribute('class', resultsContainerSelector);
+        document.body.appendChild(resultsContainer);
+
+        // set the min width of the container equals to the input box's
+        resultsContainer.style.width = inputBoxCurrent.offsetWidth + 'px';
+        resultsContainer.style.display = 'block';
+        const containerWidth = resultsContainer.offsetWidth;
+        resultsContainer.style.display = 'none';
+
+        if (containerWidth > 250) {
+            resultsContainer.classList.add('instantSearchResultsContainer--lg');
+        } else {
+            resultsContainer.classList.remove('instantSearchResultsContainer--lg');
+        }
+
+        const offsetInputBox  = inputBox.getBoundingClientRect();
+        const inputBoxLeft    = offsetInputBox.left + window.scrollX;
+        const inputBoxTop     = offsetInputBox.top + window.scrollY;
+        const containerRightX = inputBoxLeft + containerWidth;
+        const winWidth        = document.documentElement.clientWidth;
+        let leftVal;
+
+        if (containerRightX > winWidth) {
+            // If the container overflows the screen horizontally, move it backward
+            leftVal = Math.max(0, inputBoxLeft - containerRightX + winWidth);
+        } else {
+            leftVal = inputBoxLeft;
+        }
+        resultsContainer.style.left = leftVal + 'px';
+        resultsContainer.style.top  = (inputBox.offsetHeight + inputBoxTop) + 'px';
     }
-    container.style.left = leftVal + 'px';
-    container.style.top = (inputBox.offsetHeight + inputBoxTop) + 'px';
+}
+
+async function removeResultsContainer() {
+    const resultsContainer = document.querySelector(`#${resultsContainerSelector}`);
+
+    if (inputBoxCurrent && resultsContainer) {
+        await slideUp({element: resultsContainer, delay: 300, slideSpeed: 200});
+        resultsContainer.remove();
+    }
 }
