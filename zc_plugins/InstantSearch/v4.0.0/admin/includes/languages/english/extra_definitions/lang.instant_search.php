14ed110c7c77a83197054cff259af96fdb25ce39
Marco Ponchia <marco.ponchia@marcopm.net>
2023-04-17 13:27:33 +0200

    Allow ! character on image size settings

--- zc_plugins/InstantSearch/v4.0.0/admin/includes/languages/english/extra_definitions/lang.instant_search.php
+++ zc_plugins/InstantSearch/v4.0.0/admin/includes/languages/english/extra_definitions/lang.instant_search.php
@@ -12,6 +12,7 @@ $define = [
     'TEXT_INSTANT_SEARCH_CONFIGURATION_INT_VALIDATE' => 'Value must be an integer.',
     'TEXT_INSTANT_SEARCH_CONFIGURATION_FIELDS_LIST_VALIDATE' => 'The fields list must contain only the allowed values, separated by comma (without spaces or other characters).',
     'TEXT_INSTANT_SEARCH_CONFIGURATION_MAX_RESULTS_PAGE_VALIDATE' => 'The value must be an integer and not higher than 250.',
+    'TEXT_INSTANT_SEARCH_CONFIGURATION_IMG_SIZE_VALIDATE' => 'The value must be an integer (it may be followed by an exclamation mark).',
 ];
 
 return $define;
